226ec04fcc57baa1b83ec3b9947497088b430f47
Andrii-Mitko <andreymit123@gmail.com>
2026-07-01 21:38:58 +0300

    add

--- src/components/App.tsx
+++ src/components/App.tsx
@@ -1,34 +1,34 @@
 import { useState } from "react";
-import Alert from "./Alert";
-import Button from "./Button";
-import Product from "./Product";
+
+interface Values {
+  x: number;
+  y: number;
+}
 
 export default function App() {
-  const [clicks, setClicks] = useState(0);
+  const [values, setValues] = useState<Values>({ x: 0, y: 0 });
 
-  const handleClick = () => {
-    //  Використовуємо setClicks для зміни стану clicks
-    setClicks(clicks + 1);
+ 
+  
+  const updateX = () => {
+    setValues({
+      ...values,
+      x: values.x + 1,
+    });
   };
+
+  const updateY = () => {
+    setValues({
+      ...values,
+      y: values.y + 1,
+    });
+  };
+
   return (
-    <>
-      <h1>Best selling</h1>
-      <Product
-        name="Tacos With Lime"
-        imgUrl="https://images.pexels.com/photos/461198/pexels-photo-461198.jpeg?w=640"
-        price={10.99}
-      />
-      <Product
-        name="Fries and Burger"
-        imgUrl="https://images.pexels.com/photos/70497/pexels-photo-70497.jpeg?w=640"
-        price={14.29}
-      />
-      <Alert />
-      <Alert type="success" />
-      <Alert type="error" />
-      <Button variant="primary" text="Login" />
-      <Button variant="secondary" text="Follow" />
-      <button onClick={handleClick}>Current: {clicks}</button>;
-    </>
+    <div>
+      <p>x: {values.x}, y: {values.y}</p>
+      <button onClick={updateX}>Update x</button>
+      <button onClick={updateY}>Update y</button>
+    </div>
   );
 }
